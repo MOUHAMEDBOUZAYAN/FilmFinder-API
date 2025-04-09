4082af1104db4ef1c33f45eac262efac63d394fd
mouhamedbouzayan <mohammedbouzi177@gmail.com>
2025-04-09 16:24:44 +0100

    Add Page Home Sur App.jsx Pour Afficher les Films

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,5 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import MovieDetail from './pages/MovieDetail';
+
+
 
 function App() {
   return (
@@ -8,7 +12,9 @@ function App() {
         <Navbar />
         <main className="pb-10">
           <Routes>
-            
+            <Route path="/" element={<Home />} />
+            <Route path="/movie/:id" element={<MovieDetail />} />
+           
           </Routes>
         </main>
       </div>
